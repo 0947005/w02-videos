31db6c12ee807a98c61ada803ecd950cc384cb03
Fionna Elizabeth Chalmers <FionnaElizabeth@MacBook-Pro.local>
2020-09-30 11:22:12 +0100

    updated balance

--- week02_demo.py
+++ week02_demo.py
@@ -1,6 +1,7 @@
 # Initial balance of the bank account
 initial_balance = 200
 
+
 # Bank statement with all transactions for the past 6 months
 statement = [[-119.02, -56.54, 1200, -80, -12.99, -550, -167.90, -5.58, -3.54, -9.99],
              [-138.32, -67.12, 1200, -80, -12.99, -268.10, -550, -92.90, -125.65],
@@ -8,6 +9,7 @@ statement = [[-119.02, -56.54, 1200, -80, -12.99, -550, -167.90, -5.58, -3.54, -
              [-91.98, -45.65, -50, -9.99, 1200, -80, -414.22, -12.99, -550, -9.29, -67.12],
              [-159.53, -27.61, -168.45, 1200, -80, -12.99, -76.94, -550, -28.08, -27.89],
              [-141.97, 1200, -87.78, -80, -12.99, -67.92, -188.09, -550, -4.20, -13.68]]
+
 
 # Your job: pay monthly 0.5% interest to this account.
 
@@ -20,6 +22,12 @@ statement = [[-119.02, -56.54, 1200, -80, -12.99, -550, -167.90, -5.58, -3.54, -
 def update_balance(month, starting_balance):
     balance = starting_balance + sum(month)
     return balance
+
+# Toy Example
+example_month = [100,100,100,-200,-50]
+example_starting_balance = 0
+print(update_balance(example_month, example_starting_balance))
+print(update_balance(statement[0], initial_balance))
 
 def calculate_interest(balance, interest_rate):
     '''
@@ -35,11 +43,11 @@ def update_statement(statement, initial_balance, interest_rate):
     '''
     balance = initial_balance
     total_interest = 0
-    
+
     for month in statement:
         balance = update_balance(month, balance)
         total_interest = total_interest + calculate_interest(balance, interest_rate)
-    
+
     statement[-1].append(total_interest)
     balance = balance + total_interest
 
